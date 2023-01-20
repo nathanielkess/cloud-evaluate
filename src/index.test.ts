5e45a897d1a4f262f4dfe30069480fbd06b247c7
nathanielkess <hey@nathanielkessler.com>
2023-01-20 18:16:15 -0500

    removes greeter

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { Greeter } from './index';
-
-describe('Greeter', () => {
-  it('should greet', () => {
-    const greetings = Greeter('John');
-    expect(greetings).toEqual('Hello John');
-  })
-})
